bdf47ed75dc08ee4bad04a764a7bf1cc68e4d036
Your Name <you@example.com>
2024-08-31 12:00:35 -0400

    Construction Landing Page Upload

---
+++ main.js
@@ -0,0 +1,6 @@
+/*===================== YEAR CALCULATOR =====================*/
+document.addEventListener("DOMContentLoaded", function () {
+    const yearSpan = document.getElementById("year");
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = `${currentYear} BuildCon`;
+  });
